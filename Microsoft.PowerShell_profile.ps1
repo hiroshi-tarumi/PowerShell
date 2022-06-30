c4ae202bc87789ee2c3872c2d7b2e4222c81b53d
hiroshi-tarumi <tarumi8171@gmail.com>
2022-06-30 11:57:14 +0900

    first commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\pixelrobots.omp.json" | Invoke-Expression
+oh-my-posh --init --shell pwsh --config c:/users/xxx/Desktop/tarumi/oh-my-posh/themes/iterm2.omp.json | Invoke-Expression
+# oh-my-posh init pwsh --config C:\Users\xxx\AppData\Local\oh-my-posh\themes/blue-owl.omp.json | Invoke-Expression
+# oh-my-posh init pwsh --config C:\Users\xxx\themes/ohmyposhv3-v2.json | Invoke-Expression
+
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module -Name Terminal-Icons
+
+# Set-PoshPrompt -Theme blue-owl
+
+set-alias graph git-log
