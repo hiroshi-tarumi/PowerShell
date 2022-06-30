500ed1757c970789cb701d77abea23878bb69dc8
hiroshi-tarumi <tarumi8171@gmail.com>
2022-06-30 12:31:38 +0900

    ner branch stat

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\pixelrobots.omp.json" | Invoke-Expression
-oh-my-posh --init --shell pwsh --config c:/users/xxx/Desktop/tarumi/oh-my-posh/themes/iterm2.omp.json | Invoke-Expression
+# oh-my-posh --init --shell pwsh --config c:/users/xxx/Desktop/tarumi/oh-my-posh/themes/iterm2.omp.json | Invoke-Expression
 # oh-my-posh init pwsh --config C:\Users\xxx\AppData\Local\oh-my-posh\themes/blue-owl.omp.json | Invoke-Expression
 # oh-my-posh init pwsh --config C:\Users\xxx\themes/ohmyposhv3-v2.json | Invoke-Expression
 
@@ -7,5 +7,4 @@ Import-Module posh-git
 Import-Module oh-my-posh
 Import-Module -Name Terminal-Icons
 
-# Set-PoshPrompt -Theme blue-owl
-
+Set-PoshPrompt -Theme blue-owl
